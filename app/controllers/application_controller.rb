fe02925376b4165d27b144002e4d239f1117cba8
ZhaoZhenTang <tangzhaozhen8@gmail.com>
2016-09-17 03:31:58 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+    render html: "¡hola,mundo!"
+  end
+  
+  def goodbye
+    render html: "Goodbye,world!"
+  end
+  
+end
